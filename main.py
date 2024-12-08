83830c2ba6e7754912993ca7188c7f0e26288ef8
t2ance <12111327@mail.sustech.edu.cn>
2024-12-08 15:41:11 +0800

    updated

--- main.py
+++ main.py
@@ -484,7 +484,7 @@ if __name__ == "__main__":
         # configure learning rate
         bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
         if not cpu:
-            ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
+            ngpu = len(str(lightning_config.trainer.gpus).strip(",").split(','))
         else:
             ngpu = 1
         accumulate_grad_batches = lightning_config.trainer.accumulate_grad_batches or 1
